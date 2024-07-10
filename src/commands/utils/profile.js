39b81d82d1df98fb791b714bae04388c847bfa8e
BouBou <69978359+BouBouw@users.noreply.github.com>
2024-07-10 11:49:02 +0200

    Update profile.js

--- src/commands/utils/profile.js
+++ src/commands/utils/profile.js
@@ -1,4 +1,6 @@
-const { ApplicationCommandType, Colors } = require('discord.js')
+const { ApplicationCommandType, Colors, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js')
+const moment = require('moment');
+
 const Levels = require('../../../handlers/functions/database/Levels');
 const LangageManager = require('../../../handlers/langs/langageDetector');
 
@@ -9,6 +11,14 @@ module.exports = {
 execute: async (client, interaction, args, con) => {
     const currentProfile = await Levels.ProfileExperienceManager(interaction.guild, interaction.user);
 
+    const row = new ActionRowBuilder()
+    .addComponents(
+        new ButtonBuilder()
+        .setLabel(`${interaction.user.username}'s Profile`)
+        .setURL(`https://localhost:90/profile?userID=${interaction.user.id}`)
+        .setStyle(ButtonStyle.Link)
+    )
+
     interaction.reply({
         embeds: [{
             color: Colors.Purple,
@@ -18,19 +28,35 @@ execute: async (client, interaction, args, con) => {
             },
             fields: [
                 {
-                    name: `Global :`,
-                    value: `Lang : ${await LangageManager.LangageSelector(await LangageManager.UserLanguageDetector(interaction.user.id))}`
+                    name: `⚙️ Global :`,
+                    value: `
+                    ${interaction.member} (\`${interaction.member.id}\`)
+                    Created At : ${moment(interaction.user.createdAt).format('DD/MM/YYYY')}
+                    Langage : ${await LangageManager.LangageSelector(await LangageManager.UserLanguageDetector(interaction.user.id))}
+                    `
                 },
                 {
-                    name: `Level :`,
-                    value: `Rank : ${currentProfile.currentRank.currentLevel.name}\nExp : ${currentProfile.currentExp}`
+                    name: `🌎 Server :`,
+                    value: `
+                    Joined At : ${moment(interaction.user.joinedAt).format('DD/MM/YYYY')}
+                    Bot : ${interaction.user.bot ? 'True' : 'False'}
+                    Booster : ${interaction.member.premiumSince ? 'True' : 'False'}
+                    `
+                },
+                {
+                    name: `📊 Level :`,
+                    value: `
+                    Rank : ${currentProfile.currentRank.currentLevel.name}
+                    Exp : ${currentProfile.currentExp}
+                    `
                 }
             ],
             image: {
                 url: `attachment://${currentProfile.rankData.image_banner.name}`
             }
         }],
-        files: [ currentProfile.rankData.image_banner ]
+        files: [ currentProfile.rankData.image_banner ],
+        components: [ row ],
     })
     }
 }
